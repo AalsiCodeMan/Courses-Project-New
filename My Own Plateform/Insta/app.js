74631c6df5f53c554a12c87a942d98f7df6b1618
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-22 09:38:08 +0530

    Using Middleware

--- My Own Plateform/Insta/app.js
+++ My Own Plateform/Insta/app.js
@@ -2,8 +2,19 @@ const express =require('express')
 const app=express()
 const PORT=1001
 
+const customMiddleware=(req,res,next)=>{
+    console.log("MiddleWare is Running Now")
+    next()
+}
+
 app.get('/',(req,res)=>{
+    console.log("Home")
     res.send("Hello Baby")
+})
+
+app.get('/aboutus',customMiddleware,(req, res) => {
+    console.log("About")
+    res.send("About us Page")
 })
 
 app.listen(PORT,()=>{
