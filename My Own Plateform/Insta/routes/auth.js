2e1b295340211499d09c3a6c3e2a9701ce694394
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-22 10:41:46 +0530

    Getting Data to Request Body

--- My Own Plateform/Insta/routes/auth.js
+++ My Own Plateform/Insta/routes/auth.js
@@ -5,7 +5,7 @@ router.get('/',(req,res)=>{
     res.send("Hy Baby!")
 })
 
-router.post('./signup',(req,res)=>{
+router.post('/signup',(req,res)=>{
     const{name,email,password}=req.body
     if(!email || !password || !name){
         return res.status(422).json({error:"Please Fill all the fields"})
