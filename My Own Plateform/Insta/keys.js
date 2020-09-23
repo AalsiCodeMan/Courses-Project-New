4e0c0a8b068539369713f7da4bf860c6ea1a97ed
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-23 09:36:11 +0530

    uSing jwt

--- My Own Plateform/Insta/keys.js
+++ My Own Plateform/Insta/keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI: "mongodb+srv://AalsiDevil:XOHlAlOnTiyfSFau@cluster0.hebxb.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    MONGOURI: "mongodb+srv://AalsiDevil:XOHlAlOnTiyfSFau@cluster0.hebxb.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    JWT_SECRET:"TYGFTUFGFTYDESFTKGFDTDTD"
 }
--- My Own Plateform/Insta/routes/auth.js
+++ My Own Plateform/Insta/routes/auth.js
@@ -3,6 +3,9 @@ const router=express.Router()
 const mongoose=require('mongoose')
 const User=mongoose.model("User")
 const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
+const{JWT_SECRET}=require('../keys')
+
 
 router.post('/signup',(req,res)=>{
     const{name,email,password}=req.body
@@ -47,7 +50,9 @@ router.post('/signin',(req,res)=>{
         bcrypt.compare(password,savedUser.password)
         .then(doMatch=>{
             if(doMatch){
-                res.json({message:"Successfully Signed in"})
+                //res.json({message:"Successfully Signed in"})
+                const token=jwt.sign({_id:savedUser._id},JWT_SECRET)
+                res.json({token})
             }
             else{
                 return res.status(422).json({error:"Invalid password"})
