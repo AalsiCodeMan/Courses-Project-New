2e0ed0b24175b5255311dea68ea8d8fdd391134e
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-24 11:27:39 +0530

    Section 3 error

--- My Own Plateform/Insta/routes/auth.js
+++ My Own Plateform/Insta/routes/auth.js
@@ -5,7 +5,7 @@ const User=mongoose.model("User")
 const bcrypt=require('bcryptjs')
 const jwt=require('jsonwebtoken')
 const{JWT_SECRET}=require('../keys')
-const requirelogin=require('../middleware/requireLogin')
+//const requirelogin=require('../middleware/requireLogin')
 
 
 router.post('/signup',(req,res)=>{
