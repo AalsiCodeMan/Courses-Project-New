8a72877980714ce41a316637900114efeb31ab69
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-09-25 11:15:02 +0530

    Adding SignIn & SignUp UI

--- My Own Plateform/Insta/client/src/components/Navbar.js
+++ My Own Plateform/Insta/client/src/components/Navbar.js
@@ -3,11 +3,11 @@ import {Link} from 'react-router-dom'
 const Navbar=()=>{
     return(
             <nav>
-                <div className="nav-wrapper white">
-                <Link to="/" clLinkssName="brand-lLinkgo left">4Tex</Link>
+                <div className="nav-wrapper white ">
+                <Link to="/" className="brand-logo left">FourTex</Link>
                 <ul id="nav-mobile" className="right">
-                    <li><Link to="/signin">Signin</Link></li>
-                    <li><Link to="/signup">Signup</Link></li>
+                    <li><Link to="/signin">SignIn</Link></li>
+                    <li><Link to="/signup">SignUp</Link></li>
                     <li><Link to="/profile">Profile</Link></li>
                 </ul>
                 </div>
--- My Own Plateform/Insta/client/src/components/screens/Signin.js
+++ My Own Plateform/Insta/client/src/components/screens/Signin.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const Signup=()=>{
+const SignIn=()=>{
     return(
         <div className="mycard">
             < div className = "card auth-card input-field" >
             <h2>FourTex</h2>
-            <input type="text" placeholder="Name"/>
             <input type="text" placeholder="E-mail"/>
             <input type="text" placeholder="Password"/>
-             <button className="btn waves-effect waves-light #b39ddb deep-purple lighten-3">SignUp</button>
+             <button className="btn waves-effect waves-light #b39ddb deep-purple lighten-3">Login</button>
              <h5>
-                 <Link to="/signin">Already have an Account?</Link>
+                 <Link to="/signup">Don't have an Account?</Link>
              </h5>
             </div>
         </div>
@@ -19,4 +18,4 @@ const Signup=()=>{
     )
 }
 
-export default Signup
+export default SignIn
