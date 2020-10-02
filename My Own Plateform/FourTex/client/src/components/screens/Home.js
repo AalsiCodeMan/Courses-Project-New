1afeb456d09567608b759a7048ea73dc5d01dc77
kartikeya649 <kartikeyasinghs74@hotmail.com>
2020-10-02 12:32:27 +0530

    Fetching Posts

--- My Own Plateform/FourTex/client/src/components/screens/Home.js
+++ My Own Plateform/FourTex/client/src/components/screens/Home.js
@@ -1,32 +1,38 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 const Home=()=>{
+    const [data,setData]=useState([])
+    useEffect(()=>{
+        fetch('/allpost',{
+            headers:{
+                "Authorization":"Bearer"+localStorage.getItem("jwt")
+            }
+        }).then(res=>res.json())
+        .then(result=>{
+            setData(result.posts)
+        })
+    },[])
     return(
         <div className="home">
-            <div className="card home-card">
-                <h5>Zendaya</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1578998609801-595493cc256f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
-                </div>
-                <div className="card-content">
-                    <i className="material-icons" style={{color:"purple"}}>star_border</i>
-                    <h6>Title</h6>
-                    <p>This is Amazing Post</p>
-                    <input type="text" placeholder="add a comment"/>
-                </div>
-            </div>
-            <div className="card home-card">
-                <h5>Zendaya</h5>
-                <div className="card-image">
-                    <img src="https://images.unsplash.com/photo-1558900243-1e6ef4670dc3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80" />
-                </div>
-                <div className="card-content">
-                    <i className="material-icons" style={{ color: "purple" }}>star_border</i>
-                    <h6>Title</h6>
-                    <p>This is Amazing Post</p>
-                    <input type="text" placeholder="add a comment" />
-                </div>
-            </div>
+            {
+               data.map(item=>{
+                   return(
+                       <div className="card home-card" key={item._id}>
+                           <h5>{item.postedBy.name}</h5>
+                           <div className="card-image">
+                               <img src={item.photo}/>
+                           </div>
+                           <div className="card-content">
+                               <i className="material-icons" style={{ color: "purple" }}>star_border</i>
+                               <h6>{item.title}</h6>
+                                    <p>{item.body}</p>
+                               <input type="text" placeholder="add a comment" />
+                           </div>
+                       </div>
+                   )
+               }) 
+            }
+
         </div>
     )
 }
--- My Own Plateform/FourTex/routes/post.js
+++ My Own Plateform/FourTex/routes/post.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const requirelogin = require('../middleware/requireLogin')
 const Post=mongoose.model("Post")
 
-router.get('/allpost',(req,res)=>{
+router.get('/allpost',requirelogin,(req,res)=>{
     Post.find()
     .populate("postedBy","_id name")
     .then(posts=>{
